ebbadcb1f40df009a0ff840e17c6d65724dc2dbf
Lingheng Meng <linghengmeng@yahoo.com>
2019-07-18 16:10:46 -0400

    add buffer_size parameter

--- baselines/deepq_n_step/experiments/train_cartpole.py
+++ baselines/deepq_n_step/experiments/train_cartpole.py
@@ -13,7 +13,7 @@ def callback(lcl, _glb):
     return is_solved
 
 
-def main(env_name, seed, n_step, exp_name):
+def main(env_name, seed, n_step, buffer_size, exp_name):
     data_dir = osp.join(osp.dirname(osp.dirname(osp.dirname(osp.dirname(osp.dirname(osp.abspath(__file__)))))),
                         'openai_dqn_classic', datetime.datetime.now().strftime("openai-%Y-%m-%d-%H-%M-%S_") + exp_name)
     logger.configure(dir=data_dir)
@@ -25,7 +25,7 @@ def main(env_name, seed, n_step, exp_name):
         lr=1e-3,
         total_timesteps=100000,
         n_step=n_step,
-        buffer_size=50000,
+        buffer_size=buffer_size,
         exploration_fraction=0.1,
         exploration_final_eps=0.02,
         print_freq=10
@@ -40,5 +40,8 @@ if __name__ == '__main__':
     parser.add_argument('--seed', type=int, default=0)
     parser.add_argument('--exp_name', type=str, default='dqn_n_step')
     parser.add_argument('--n_step', type=int, default=1)
+    parser.add_argument('--buffer_size', type=int, default=50000)
     args = parser.parse_args()
-    main(env_name=args.env_name, seed=args.seed, n_step=args.n_step, exp_name=args.exp_name)
+    main(env_name=args.env_name, seed=args.seed,
+         n_step=args.n_step, buffer_size=args.buffer_size,
+         exp_name=args.exp_name)
